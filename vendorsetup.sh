bfb79dc6bf0714d5fcd62cbe10202f055be02b0f
itsdrew85 <martin.andrew@icloud.com>
2025-01-16 20:40:27 -0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_ellis-user
-add_lunch_combo omni_ellis-userdebug
-add_lunch_combo omni_ellis-eng
+add_lunch_combo twrp_ellis-user
+add_lunch_combo twrp_ellis-userdebug
+add_lunch_combo twrp_ellis-eng
